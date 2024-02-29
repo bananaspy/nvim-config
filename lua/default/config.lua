6b821a4d902305de740e4c362e4296f2746bbc13
Vitaliy Starov <vitaliy@starov.dev>
2024-02-29 19:39:18 +0000

    enabled netrw banner by default

--- lua/default/config.lua
+++ lua/default/config.lua
@@ -1,4 +1,4 @@
-vim.g.netrw_banner = 0
+vim.g.netrw_banner = true
 
 vim.o.cursorline = true
 
@@ -13,4 +13,3 @@ vim.o.softtabstop = 2
 vim.o.shiftwidth = 2
 vim.o.expandtab = true
 vim.o.smartindent = true
-
